dc24dbbeea3b3278b9fb42a520399a119b09bd23
Taurin190 <taura.koichi@gmail.com>
2024-11-11 08:44:17 +0900

    add socket server

--- cmd/geo-server/main.go
+++ cmd/geo-server/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
+	"os"
 
 	"github.com/Taurin190/geofrontdb/internal/config"
 	log "github.com/sirupsen/logrus"
+)
+
+const (
+	TCP_PROTOCOL = "tcp"
 )
 
 func init() {
@@ -15,6 +21,54 @@ func init() {
 func main() {
 	log.Info("Starting Geofront Server...")
 	config := config.NewServerConfig()
-	log.Infof("Server started on port %s", config.Port)
-	fmt.Println("Hello, World!")
+
+	tcpAddress, err := net.ResolveTCPAddr(TCP_PROTOCOL, fmt.Sprintf("localhost:%s", config.Port))
+	if err != nil {
+		log.Fatalf("Error resolving TCP address: %+v", err)
+	}
+
+	listener, err := net.ListenTCP(TCP_PROTOCOL, tcpAddress)
+	if err != nil {
+		log.Fatalf("Error listening: %+v", err)
+	}
+	log.Infof(fmt.Sprintf("[Server][Parent] *net.TCPListener Address: %s", listener.Addr().String()))
+
+	for {
+		clientConn, err := listener.Accept()
+		if err != nil {
+			log.Warnf("Error accepting: %+v", err)
+			continue
+		}
+		go handler(clientConn)
+	}
+}
+
+func handler(conn net.Conn) {
+	pid := os.Getpid()
+
+	la := conn.LocalAddr().String()
+	ra := conn.RemoteAddr().String()
+	connInfo := fmt.Sprintf("[SERVER] pid:%d, localAddress:%s, remoteAddress:%s", pid, la, ra)
+	log.Println(connInfo)
+
+	for {
+		request := make([]byte, 4096)
+
+		// クライアント X から通信が来るまで待機する
+		readLen, err := conn.Read(request)
+		if err != nil {
+			log.Printf("Error reading: %s", err.Error())
+			break
+		}
+
+		// クライアントが接続を切った
+		if readLen == 0 {
+			break
+		}
+
+		conn.Write([]byte("[From][Server] Hello! Your message is " + string(request)))
+		log.Printf("%s sent to client message: %s", connInfo, string(request))
+	}
+
+	conn.Close()
 }
